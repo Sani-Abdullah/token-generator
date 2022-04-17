543e0fa6139b39e90771060f6b7d7aaebb96c237
abdul <heatwavemachine@gmail.com>
2022-04-17 15:47:02 +0100

    up to permutate

--- tokencipher.py
+++ tokencipher.py
@@ -1,5 +1,7 @@
+from functools import reduce
+import string
 from keygen import generate_decoder_key
-from tokengen import bin_pad, bin_str, generate_token_block
+from tokengen import bin_pad, bin_str, concat_str, generate_token_block
 
 
 def start_point(decoder_key, token_block):
@@ -25,7 +27,7 @@ def substitute(bits64_string):
     }
 
     nibbles = nibbleate(bits64_string)
-    enumerated_nibbles = list(zip(list(range(len(nibbles))), nibbles))
+    enumerated_nibbles = list(zip(range(len(nibbles)), nibbles))
 
     for i, nibble in enumerated_nibbles:
         if nibble[0] == '0':
@@ -36,8 +38,19 @@ def substitute(bits64_string):
             nibbles[i] = table[i]
 
     substituted_nibbles = list(map(lambda x: bin_pad(bin_str(x), 4), nibbles))
-
-    return substituted_nibbles
+    concatenated_substituted_nibbles = reduce(concat_str, substituted_nibbles)
+    return concatenated_substituted_nibbles
 
 def permutate(bits64_string):
-    table = []
+    table = [57, 9, 61, 53, 2, 14, 4, 38, 52, 29, 28, 45, 15, 11, 20, 3, 35, 17, 50, 40, 59, 16, 43, 47, 25, 18, 62, 37, 33, 24, 55, 22, 30, 63, 23, 0, 19, 26, 32, 7, 31, 36, 13, 46, 44, 48, 56, 39, 8, 60, 27, 49, 10, 42, 6, 5, 1, 12, 58, 54, 41, 34, 21, 51]
+    initial_permutation = [i for i in bits64_string]
+    final_permutation = list(range(64))
+
+    for i in list(range(64)):
+        final_permutation[table[i]] = initial_permutation[i]
+
+    joined_permutated_bits = ''.join(final_permutation)
+    
+    return joined_permutated_bits
+
+permutate(start_point)
